842eecbe6c655ee0d656bdf26b6dd5138cdae183
doug@luna <doug@totem9.net>
2019-05-13 22:33:19 +0100

    working on command types

--- output/main.cpp
+++ output/main.cpp
@@ -7,31 +7,57 @@ using namespace std;
 
 namespace build {
 
+Node b_block(const Node& ast);
+
+Node b_expr(const Node& ast) {
+	Node e = {"()", {
+		{"i32.const"},
+		{ ast.get(0).val }  // TODO: better job here
+	}};
+	return e;
+}
+
 Node b_if(const Node& ast) {
-	return {};
+	return {"()", {
+		{"if"}, 
+		b_expr ( ast.get("expr")  ),
+		b_block( ast.get("block") )
+	}};
 }
 
 Node b_block(const Node& ast) {
 	Node block = {"()", { 
 		{"block"}
 	}};
+	// parse each block command
 	for (auto& ln : ast.list)
-		if (ln.val == "if") ;
+		if (ln.val == "if") block.list.push_back( b_if(ln) );
 		else throw string("unexpected in block: "+ln.val);
 	return block;
 }
 
 Node b_dim_block(const Node& ast) {
-	Node dblock = {"()"};
-	Node assign = {"()"};
-	for (auto& ln : ast.list)
-		; // ... TODO
-	return dblock;
+	Node dim = {"()"};
+	// define dim locals
+	for (auto& d : ast.list)
+		dim.list.push_back(
+			{"()", {
+				{"local"},
+				{ "$"+d.get("name").get(0).val }
+			}} );
+	// set initial values
+	for (auto& d : ast.list)
+		dim.list.push_back(
+			{"()", {
+				{"local.set"}, 
+				{ "$"+d.get("name").get(0).val },
+				b_expr( d.get("expr") )
+			}} );
+	return dim;
 }
 
 Node b_function(const Node& ast) {
 	auto name = ast.get("name").get(0).val;
-	auto block = b_block(ast.get("block"));
 	// function template
 	Node func = {"()", { 
 		{"func"}, {"$"+name}, 
@@ -39,24 +65,10 @@ Node b_function(const Node& ast) {
 			{"()", { {"result"}, {"i32"} }}, // return value
 	}};
 	// define dim locals
-	for (auto& d : ast.get("dim_block").list)
-		func.list.push_back(
-			{"()", {
-				{"local"},
-				{ "$"+d.get("name").get(0).val }
-			}} );
-	// set initial values
-	for (auto& d : ast.get("dim_block").list)
-		func.list.push_back(
-			{"()", {
-				{"local.set"}, 
-				{ "$"+d.get("name").get(0).val },
-				{"()", {
-					{"i32.const"},
-					{ d.get("expr").get(0).val }
-				}}
-			}} );
+	auto dim = b_dim_block(ast.get("dim_block"));
+	func.list.insert(func.list.end(), dim.list.begin(), dim.list.end());
 	// main block
+	auto block = b_block(ast.get("block"));
 	func.list.push_back(block);
 	// append default return value
 	func.list.push_back(
@@ -73,7 +85,8 @@ Node b_module(const Node& ast) {
 	// globals
 	// (global $b (export "b") i32 (i32.const 0))
 	// functions
-	// (function $f)
+	for (auto& f : ast.get("functions").list)
+		module.list.push_back( b_function(f) );
 	return module;
 }
 
@@ -101,22 +114,22 @@ int main() {
 					}}
 				}},
 				{"block", {
-					{"set", {
-						{"var", { {"a"} }},
-						{"expr", {
-							{"+", {
-								{"var", { {"a"} }},
-								{"number", { {"1"} }}
-							}}
-						}}
-					}}
+					// {"set", {
+					// 	{"var", { {"a"} }},
+					// 	{"expr", {
+					// 		{"+", {
+					// 			{"var", { {"a"} }},
+					// 			{"number", { {"1"} }}
+					// 		}}
+					// 	}}
+					// }}
 				}}
 			}}
 		}}
 	}};
 
 	try {
-		Node prog = build::build_function(func);
+		Node prog = build::b_function(func);
 		node_show(prog);
 	}
 	catch (const string& err) {
