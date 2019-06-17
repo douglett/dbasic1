70afe02bd68aeb540029cbd6ae9fa732a48969be
Douglas Inman <Douglas.Inman@strategic-i.com>
2019-06-17 17:55:29 +0100

    small update

--- lexxer/lexxer.hpp
+++ lexxer/lexxer.hpp
@@ -8,10 +8,18 @@ struct Lexxer : ParseTools {
 		Rule::make("NUMBER", "D+")
 	};
 
-	int run(const Rule& rule, const std::string& str) {
-		std::string res;
+	// int run(const Rule& rule, const std::string& str) {
+	// 	int pos = 0;
+	// 	return run(rule, str, pos);
+	// }
+	int run(const Rule& rule, const std::string& str, const int pos=0) {
+		int len = 0, l = 0;
 		for (const auto& r : rule.rule.list) {
-			std::cout << r.val << std::endl;
+			printf("running rule: %s\n", r.val.c_str());
+			if      (r.val == "L"  && isletter(str[pos])) len++;
+			else if (r.val == "D"  && isdigit(str[pos]) ) len++;
+			// else if (r.val == "()" && (l = run(r, str, pos)) > -1) len+=l;
+			return -1;
 		}
 		return 0;
 	}
