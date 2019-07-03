27109f574c66ef2d5e501a12a9d9acd976965f20
doug@xxx <doug@totem9.net>
2019-07-03 23:32:18 +0100

    working on output

--- src/parsing/compiler.hpp
+++ src/parsing/compiler.hpp
@@ -14,21 +14,33 @@ struct Compiler {
 			{"module"}, {"$dbasic_program"}
 		}};
 		for (auto& fn : ast.list)
-			if   (fn.val == "function") p.list.push_back(function(fn));
+			if   (fn.val == "function") p.push(function(fn));
 			else throw std::string("expected function");
 		return p;
 	}
 
 	Node function(const Node& fn) {
-		auto name = fn.get("name").get(0).val;
+		auto& name = fn.get("name").get(0).val;
 		Node f = {"()", {
 			{"function"},
 			{"$"+name},
 			{"(export \""+name+"\")"},
 			{"(result i32)"}
 		}};
-//		fn.get("block").list.push_back
-		f.list.push_back({"(i32.const 0)"});
+//		fn.get("block").push
+		block(fn.get("block"), f);
+		f.push({"()", { {"i32.const 0"} }}); // return value
 		return f;
+	}
+
+	void block(const Node& bl, Node& parent) {
+		for (auto& b : bl.list)
+			if   (b.val == "dim") parent.push(dim(b));
+			else throw std::string("unexpected in block");
+	}
+
+	Node dim(const Node& d) {
+		auto& name = d.get("name").get(0).val;
+		return {"()", { {"local $"+name+" i32"} }};
 	}
 };
