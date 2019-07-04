600ef092f75b57a94961d17c1c1732a4b1bcfcf0
doug@xxx <doug@totem9.net>
2019-07-04 22:32:16 +0100

    reworking the lexxer

--- lexxer/lexxer.hpp
+++ lexxer/lexxer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "parsetools.hpp"
 #include "rule.hpp"
+#include <sstream>
 
 /**
  * Lexxer - takes input string and returns a token string
@@ -9,49 +10,72 @@ struct Lexxer : ParseTools {
 	std::vector<Rule> rules = {
 		Rule::make("WS", "S*"),
 		Rule::make("NUMBER", "D+"),
-		Rule::make("IDENTIFIER", "L(L|D)*")
+		Rule::make("IDENTIFIER", "L(L|D)*"),
+		Rule::make("main", "(WS IDENTIFIER WS)+")
 	};
+	std::stringstream input;
 
 	Rule& findrule(const std::string& rulename) {
 		for (auto& r : rules)
 			if (r.name == rulename) return r;
-		throw std::string("missing lexxer rule: "+rulename);
+		auto err = "missing lexxer rule: "+rulename;
+		std::cerr << err << std::endl;
+		exit(1);
 	}
 
-	int run(const std::string& rulename, const std::string& str, const int pos=0) {
+	int showerr(const std::string& msg) {
+		fprintf(stderr, "[%s] error at position [%d]\n",
+			msg.c_str(),
+			(int)input.tellg() );
+		return 0;
+	}
+
+	int run(const std::string& rulename) {
 		Rule& rule = findrule(rulename);
-		return runsub(rule.rule, str, pos);
+		int pos = input.tellg();
+		printf("running rule: %s\n", rulename.c_str());
+		if (runsub(rule.rule)) {
+//			printf("  [%s]\n", 
+			return 1;
+		}
+		return showerr(rulename);
 	}
 
-	int runsub(const Node& rule, const std::string& str, const int pos=0) {
-		int len = 0, l = 0;
-		// printf("running subrule: %s  [%s] [%d]\n", rule.val.c_str(), str.c_str(), pos);
-//		if      (pos >= (int)str.length()) return -1;
-		if      (false) ;
-		else if (rule.val == "S") return !iseof(str, pos) && isspace(str[pos])  ? 1 : -1;
-		else if (rule.val == "L") return !iseof(str, pos) && isletter(str[pos]) ? 1 : -1;
-		else if (rule.val == "D") return !iseof(str, pos) && isdigit(str[pos])  ? 1 : -1;
-		else if (rule.val == "|") {
-			if ( (l = runsub(rule.list[0], str, pos)) > -1 ) return l;
-			if ( (l = runsub(rule.list[1], str, pos)) > -1 ) return l;
-			return -1;
-		}
-		else if (rule.val == "*") {
-			while ( (l = runsub(rule.list[0], str, pos + len)) > 0 ) len += l;
-			return len;
-		}
-		else if (rule.val == "+") {
-			while ( (l = runsub(rule.list[0], str, pos + len)) > 0 ) len += l;
-			return len >= 1 ? len : -1;
-		}
-		else if (rule.val == "()") {
-			for (const auto& r : rule.list) {
-				l = runsub(r, str, pos + len);
-				if (l == -1) return -1;
-				len += l;
-			}
-			return len;
-		}
-		return run(rule.val, str, pos);
+	int runsub(const Node& rule) {
+		// printf("[%s]\n", rule.val.c_str());
+		if (rule.val == "S") return isspace(input.peek() ) ? (input.get(), 1) : 0;
+		if (rule.val == "L") return isletter(input.peek()) ? (input.get(), 1) : 0;
+		if (rule.val == "D") return isdigit(input.peek() ) ? (input.get(), 1) : 0;
+		if (rule.val == "()") return run_brackets(rule);
+		if (rule.val == "+") return run_plus(rule);
+		if (rule.val == "*") return run_mul(rule);
+		if (rule.val == "|") return run_or(rule);
+		return run(rule.val); // run rule by name
+	}
+
+	int run_brackets(const Node& rule) {
+		for (auto r : rule.list)
+			if (!runsub(r)) return 0;
+		return 1;
+	}
+
+	int run_plus(const Node& rule) {
+		if (!runsub(rule.list[0])) return 0;
+		return run_mul(rule);
+	}
+
+	int run_mul(const Node& rule) {
+		int pos = input.tellg();
+		while (runsub(rule.list[0]))
+			pos = input.tellg();
+		input.seekg(pos);
+		return 1;
+	}
+
+	int run_or(const Node& rule) {
+		int pos = input.tellg();
+		if (runsub(rule.list[0])) return 1;
+		input.seekg(pos);
+		return runsub(rule.list[1]);
 	}
 };
--- lexxer/main.cpp
+++ lexxer/main.cpp
@@ -1,36 +1,10 @@
 #include "lexxer.hpp"
-#include "astparser.hpp"
 #include <iostream>
 
 int main() {
 	printf("hello world\n");
 
-	ASTParser par;
-//	par.test();
-	try {
-		ParseTools::Node n;
-		int i = par.runrule("test2", "asd bacon cheese on toast", 0, n);
-		printf("result: %d\n", i);
-		ParseTools::shownode(n);
-	} catch(const std::string& e) {
-		fprintf(stderr, "%s\n", e.c_str());
-	}
-
-
-	// Lexxer l;
-	// printf("running...\n");
-
-	// std::string input = "test  123";
-	// std::vector<std::string> outp, rules = {
-	// 	"IDENTIFIER", "WS", "NUMBER"
-	// };
-
-	// int i = 0, j = 0;
-	// for (const auto& r : rules) {
-	// 	j = l.run(r, input, i);
-	// 	auto res = input.substr(i, j > 0 ? j : 0);
-	// 	printf("%s: [%d] [%s]\n", r.c_str(), j,res .c_str());
-	// 	if (j == -1) break;
-	// 	i += j;
-	// }
+	Lexxer lex;
+	lex.input.str("asd bacon cheese");
+	lex.run("main");
 }
